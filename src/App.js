c354bef16a9061e6e186d852c3c7c44dda6a3dbb
Airiel Altemara <vsokord#gmail.com>
2023-07-22 16:02:36 -0700

    Initial Setup of Todo App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    }
+  ])
+
+  const [value, setValue] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    const newToDos = [...todos, { text: value, isCompleted: false }];
+    setTodos(newToDos);
+    setValue('')
+  }
+
+  const removeToDo = e => {
+    const index = Number(e.target.id)
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>To Do</h1>
+      {todos.map((todo, i) => (
+        <div className="todo" key={i} id={i} onClick={removeToDo}>{todo.text}</div>
+      ))}
+      {/* form should be broken out into another componenet */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          className='input'
+          value={value}
+          placeholder="Add ToDo .."
+
+          onChange={e => setValue(e.target.value)}
+        />
+      </form>
+    </>
   );
 }
 
